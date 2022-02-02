220ccd6eb2063de783a63fe9c8bfa8f2713653ff
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-02 15:46:37 -0500

    added title and comment to card

--- LearnByDoing/CardView.swift
+++ LearnByDoing/CardView.swift
@@ -7,17 +7,26 @@
 
 import SwiftUI
 
-// MARK: Card
+
 struct CardView: View {
     
     // MARK:  Properties
-    
     var gradient: [Color] = [Color("Color01"), Color("Color02")]
     
     
+    // MARK: Card
     var body: some View {
         ZStack {
-            Text("CARD")
+            Image("developer-no1")
+            
+            VStack {
+                Text("SwiftUI")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                Text("Better Apps, Less Code")
+            }
         }
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient:  Gradient(colors: gradient), startPoint: .top, endPoint: .bottom))
