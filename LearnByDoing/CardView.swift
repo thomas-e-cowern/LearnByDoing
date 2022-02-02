5d4d28605d127cfc9a3c587117db2282b89539bf
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-02 13:54:54 -0500

    added card view

---
+++ LearnByDoing/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  LearnByDoing
+//
+//  Created by Thomas Cowern New on 2/2/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
