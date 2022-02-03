23f1db757c90e21afceeeb11e72d9e969fd400fb
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-03 10:27:06 -0500

    updated card view with data

--- LearnByDoing/CardView.swift
+++ LearnByDoing/CardView.swift
@@ -11,21 +11,20 @@ import SwiftUI
 struct CardView: View {
     
     // MARK:  Properties
-    var gradient: [Color] = [Color("Color01"), Color("Color02")]
-    
+    var card: Card
     
     // MARK: Card
     var body: some View {
         ZStack {
-            Image("developer-no1")
+            Image(card.imageName)
             
             VStack {
-                Text("SwiftUI")
+                Text(card.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .multilineTextAlignment(.center)
-                Text("Better Apps, Less Code")
+                Text(card.headline)
                     .font(.footnote)
                     .fontWeight(.regular)
                     .foregroundColor(Color.white)
@@ -38,7 +37,7 @@ struct CardView: View {
                 print("Button was tapped")
             }) {
                 HStack {
-                    Text("Learn".uppercased())
+                    Text(card.callToAction.uppercased())
                         .fontWeight(.heavy)
                         .foregroundColor(Color.white)
                     .accentColor(Color.white)
@@ -48,14 +47,14 @@ struct CardView: View {
                 }
                 .padding(.vertical)
                 .padding(.horizontal, 24)
-                .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .leading, endPoint: .trailing))
+                .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .leading, endPoint: .trailing))
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
             .offset(y: 210)
         }
         .frame(width: 335, height: 545)
-        .background(LinearGradient(gradient:  Gradient(colors: gradient), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient:  Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
     }
@@ -63,7 +62,7 @@ struct CardView: View {
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(card: cardData[5])
             .previewLayout(.sizeThatFits)
     }
 }
--- LearnByDoing/ContentView.swift
+++ LearnByDoing/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center, spacing: 20) {
                 ForEach(0 ..< 5) { item in
-                    CardView()
+//                    CardView()
+                    Text("Card")
                 }
             }
             .padding(20)
